4073b5fe9201ac80f398dec002e438dfb3738f10
Scott Munro <scottmunro@utexas.edu>
2017-02-10 12:52:42 -0600

    simple file creation and prevent overriding if it already exists

--- new_post.sh
+++ new_post.sh
@@ -20,5 +20,12 @@ POST_NAME=${POST_NAME:1}
 POST_FILE_NAME=${POST_FILE_NAME:1}
 POST_FILE_NAME="$POSTS_DIR$DATE-$POST_FILE_NAME.md"
 
-echo $POST_NAME
-echo $POST_FILE_NAME
+if [ -f $POST_FILE_NAME ]
+then
+    echo "Blog post with name already exists. Please delete first"
+else
+    echo $POST_NAME
+    echo $POST_FILE_NAME
+    touch $POST_FILE_NAME
+fi
+
