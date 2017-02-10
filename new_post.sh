69230e251f02e75df8d2b0de9e5c8d245a0daf94
Scott Munro <scottmunro@utexas.edu>
2017-02-10 12:48:08 -0600

    script now concatenates args correctly and builds correct filename

--- new_post.sh
+++ new_post.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
+if [ $# -eq 0 ]; then
+    echo "Please provide post name as arguments"
+    exit 1
+fi
 
-DAY=$(date +%d)
-MONTH=$(date +%m)
-YEAR=$(date +%y)
+POSTS_DIR="_posts/"
+DATE=$(date +%F)
 
-echo $MONTH/$DAY/$YEAR
+POST_NAME=""
+POST_FILE_NAME=""
+
+for cmdarg in "$@"
+do
+    POST_NAME+=" "$cmdarg
+    POST_FILE_NAME+="-"$cmdarg
+done
+
+POST_NAME=${POST_NAME:1}
+POST_FILE_NAME=${POST_FILE_NAME:1}
+POST_FILE_NAME="$POSTS_DIR$DATE-$POST_FILE_NAME.md"
+
+echo $POST_NAME
+echo $POST_FILE_NAME
